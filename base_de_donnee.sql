67c8004a72124f76a5f4d222ed55e6c8a39e43bb
sneakyPeat <david.ducluzeaud@live.fr>
2018-01-04 18:42:13 +0100

    change smallint(5) to int(10)

--- base_de_donnee.sql
+++ base_de_donnee.sql
@@ -1,7 +1,7 @@
 DROP TABLE subcategories;
-DROP TABLE product;
+DROP TABLE products;
 DROP TABLE categories;
-DROP TABLE replaced_products;
+DROP TABLE Replacement_products;
 
 CREATE TABLE Categories (
     category_name VARCHAR(255) NOT NULL PRIMARY KEY
@@ -9,12 +9,12 @@ CREATE TABLE Categories (
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE Replacement_products (
-  id_product_replacement SMALLINT(5) AUTO_INCREMENT PRIMARY KEY,
-  product_id smallint(5) NOT NULL
+  id_product_replacement INT(10) AUTO_INCREMENT PRIMARY KEY,
+  product_id INT(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE Products (
-    id_product SMALLINT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id_product INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     product_name VARCHAR(255),
     quantity VARCHAR(255),
     url_text VARCHAR(255),
@@ -26,16 +26,16 @@ CREATE TABLE Products (
     additives_number VARCHAR(5),
     additives TEXT,
     nutrition_score CHAR(1),
-    product_replacement_id SMALLINT(5),
+    product_replacement_id INT(10),
     added_date datetime,
-    subcategory_id SMALLINT(5) UNSIGNED,
+    subcategory_id INT(10) UNSIGNED,
     KEY fk_replacement_product_id (product_replacement_id),
     CONSTRAINT fk_replacement_product_id FOREIGN KEY (product_replacement_id) REFERENCES replacement_products(id_product_replacement)
 )engine=InnoDb
 DEFAULT CHARACTER SET = utf8;
 
 CREATE TABLE Subcategories(
-    id_subcategory SMALLINT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    id_subcategory INT(10) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     subcategory_name VARCHAR(255),
     name_category VARCHAR(255) NOT NULL,
     KEY fk_name_category(name_category),
